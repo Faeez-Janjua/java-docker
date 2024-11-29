c22bcd289b0ccf7af0beb15d0415afce57e7b86b
Faeez-Janjua <faeezateeq@gmail.com>
2024-11-29 23:50:15 +0500

    student detail

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Faeez";
+        int age = 21;
+        System.out.println("Student name is: "+name+"\nStudent age is: "+age);
+    }
+}
